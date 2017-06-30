d7f3d470da911a3a6d24b52e6edbca501bf25232
Adam Roynon <acroynon@gmail.com>
2017-06-30 21:12:19 +0100

    add Algorithms > Implementation > The Hurdle Race

---
+++ Algorithms/Implementation/The Hurdle Race/The Hurdle Race.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] height = new int[n];
+        for(int height_i=0; height_i < n; height_i++){
+            height[height_i] = in.nextInt();
+        }
+        // your code goes here
+        int maxHeight = 0;
+        for(int i : height){
+            maxHeight = Math.max(maxHeight, i);
+        }
+        int noDrinks = Math.max(maxHeight - k, 0);
+        System.out.println(noDrinks);
+    }
+}
